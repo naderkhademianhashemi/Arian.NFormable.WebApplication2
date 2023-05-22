6ac08288a7fe7622eff71afc48d68b83feff32af
PC 2 <PC 2@DESKTOP-8ELQAGK>
2023-05-22 12:08:33 +0430

    Add project files.

---
+++ Arian.NFormable.WebApplication2/Pages/NFormable.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SimpleForm.master" AutoEventWireup="true" CodeBehind="NFormable.aspx.cs" Inherits="PayamHannan.Arian.Management.Web.NDR.NFormable" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="SimpleFormPlace" runat="server">
+    <script src="../Scripts/NDR/NFormable.js"></script>
+    <div class="grid" id="mainPage">
+
+
+    </div>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ModalsContent" runat="server">
+</asp:Content>
---
+++ Arian.NFormable.WebApplication2/Pages/NFormable.ts
@@ -0,0 +1,53 @@
+﻿class NFormable extends Card {
+    constructor($mainPage: JQuery) {
+        super(' ');
+
+        this.$card.appendTo($mainPage);
+
+        var $row = $('<div class="row" />').appendTo(this.$body);
+        var $col4 = $('<div class="col col-4" />').append('&nbsp;').appendTo($row);
+        var $col8 = $('<div class="col col-8 text-left" />').appendTo($row);
+
+        var $actions = $('<div class="display-inline-block" />').appendTo($col4);
+
+
+        Button("NDR", (doc) => {
+            var model = {
+                reportType: "AtfSticker"
+            };
+            var pform: Formable<any, any> = new Formable<any, any>(model, [{
+                fieldOptions: [[
+                    new FieldOption({
+                        name: "reportType",
+                        title: "resource.ReportType",
+                        type: InputTypes.select,
+                        options: ["AtfSticker", "Barcode", "LendingCard", "RegCard"],
+                        optionText: k => k,
+                        optionValue: k => k
+                    }),
+                ]]
+            }],
+                [
+                    {
+                        text: resource.Execute, click: (_ps) => {
+
+                            new ModalReportViewer(_ps.reportType, { ItemId: "ItemId" })
+
+                        }
+                    },
+                    { text: resource.Cancel, theme: "danger", click: () => pform.modal.close() }
+                ],
+                null, true);
+
+            pform.Fire();
+            pform.modalSize(ModalSize.xs)
+
+        }).appendTo($actions.append(' '));
+
+    }
+}
+
+$(function () {
+    var btnNdr = new NFormable($("#mainPage"));
+
+})
